f515e0633cd5b820297e5ebf9ac5b151e7df145b
Matteo Vettosi <matteo.vettosi@gmail.com>
2021-04-19 20:14:58 +0100

    Initial commit

---
+++ app/src/main/java/com/urban/androidhomework/CharacterFragment.kt
@@ -0,0 +1,26 @@
+package com.urban.androidhomework
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class CharacterFragment : Fragment() {
+    private var name: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            name = it.getString("name")
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_character, container, false)
+        v.findViewById<TextView>(R.id.selected_name).setText("Selected name: " + name)
+        return v
+    }
+}
